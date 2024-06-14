d271abf509deebea371c506012158ee1f780a49f
lokytech5 <lokytech5@gmail.com>
2024-06-14 10:25:14 +0100

    Added daisyui package and set up daisyui theme.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -15,6 +15,11 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["sunset"],
+  },
 }
 export default config
