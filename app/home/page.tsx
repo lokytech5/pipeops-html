504bd4820e66f8e9145ead4850b8a92360d8cfc9
lokytech5 <lokytech5@gmail.com>
2024-06-14 11:33:09 +0100

    Added navbar code to home page component and make homepage visible on screen

---
+++ app/home/page.tsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import Navbar from '../components/Navbar'
+
+const HomePage = () => {
+  return (
+    <main>
+        <div className="drawer drawer-end">
+      <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
+      <div className="drawer-content flex flex-col">
+                    <Navbar />
+                   
+      </div>
+      <div className="drawer-side z-50">
+        <label htmlFor="my-drawer-3" aria-label="close sidebar" className="drawer-overlay"></label>
+        <ul className="menu p-4 w-80 min-h-full bg-base-200 text-base-content flex flex-col justify-center items-center space-y-4 no-underline rounded-box">
+          {/* Sidebar content here */}
+         <li className="text-center">
+              <a className="cursor-pointer">Home</a>
+            </li>
+        </ul>
+      </div>
+    </div>
+
+    </main>
+  )
+}
+
+export default HomePage
