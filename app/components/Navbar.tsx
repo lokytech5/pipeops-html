50ca0b74d984402e419615101243020c33845661
lokytech5 <lokytech5@gmail.com>
2024-06-14 16:33:19 +0100

    redesign application homepage to use nice colour layout

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     const logo = '/images/app-logo.jpg'
   return (
     <div className="fixed top-0 left-0 right-0 z-50 p-4">
-      <div className="navbar bg-base-200 rounded-lg shadow-lg mx-auto">
+      <div className="navbar bg-primary rounded-lg shadow-lg mx-auto">
         <label htmlFor="my-drawer-3" className="btn btn-ghost lg:hidden">
           <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" />
@@ -27,7 +27,7 @@ const Navbar = () => {
           <div className="dropdown dropdown-end">
             <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
               <div className="w-10 rounded-full">
-                <Image alt="Avatar" src={avatar} width={200} height={200} />
+                <Image alt="Avatar" src={avatar} width={200} height={200} className="bg-white" />
               </div>
             </label>
             <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-50 p-2 shadow bg-base-300 rounded-box w-52 absolute right-0">
