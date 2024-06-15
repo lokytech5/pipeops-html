55703c87fa3d79e94bb21c85d229da0b48fa0643
lokytech5 <lokytech5@gmail.com>
2024-06-15 15:04:18 +0100

    import before after to homepage component

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import Image from 'next/image'
 import ServicesSection from './ServicesSection'
 import Footer from '../components/Footer'
+import BeforeAfterSection from './BeforeAfterSection'
 
 const HomePage = () => {
   const logo = '/images/app-logo.jpg'
@@ -42,27 +43,7 @@ const HomePage = () => {
           </div>
           {/* End of Hero section */}
 
-          {/* Start of Before After Section */}
-
-          <section className="py-12 bg-base-100">
-      <div className="container mx-auto px-4">
-        <h2 className="text-4xl font-bold text-center text-primary mb-8">The Effects of Our Work</h2>
-        <div className="flex flex-col lg:flex-row items-center">
-          <div className="flex-1 p-6">
-            <h3 className="text-2xl font-semibold">Before & After</h3>
-            <p className="mt-4 text-gray-600">
-              Each of our patients undergoes a metamorphosis of their smile.
-            </p>
-            
-          </div>
-          <div className="flex-1 w-full mt-8 lg:mt-0 flex justify-center">
-            <div className="relative w-full h-64 lg:h-96">
-              <Image src={beforeAfterImage} alt="Before and After Teeth Whitening" layout="fill" objectFit="contain" className="rounded-lg shadow-xl" />
-            </div>
-          </div>
-        </div>
-      </div>
-    </section>
+          <BeforeAfterSection/>
 
          <ServicesSection/>
 
