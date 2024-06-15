ae51d83082a1117cf98b207abff8fa1341b2079d
lokytech5 <lokytech5@gmail.com>
2024-06-15 14:59:46 +0100

    Added service section to home page component

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Link from 'next/link'
 import Image from 'next/image'
+import ServicesSection from './ServicesSection'
 
 const HomePage = () => {
   const logo = '/images/app-logo.jpg'
@@ -63,33 +64,7 @@ const HomePage = () => {
     </section>
           {/* End of After Section */}
 
-          {/* Service section */}
-
-          <section className="py-12 bg-base-100">
-      <div className="container mx-auto px-4">
-        <h2 className="text-4xl font-bold text-center text-primary mb-8">Our Services</h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
-          {services.map((service, index) => (
-            <div key={index} className="card bg-base-200 shadow-xl hover:shadow-2xl transition duration-500 transform hover:-translate-y-1">
-              <figure className="px-10 pt-10">
-                <Image src={service.icon} alt={service.title} width={80} height={80} className="rounded-xl" />
-              </figure>
-              <div className="card-body items-center text-center">
-                <h3 className="card-title text-xl font-semibold">{service.title}</h3>
-                <p className="text-gray-600">{service.description}</p>
-                <div className="card-actions mt-4">
-                  <Link href={service.link}>
-                    <button className="btn btn-primary">Learn More</button>
-                  </Link>
-                </div>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
-    </section>
-
-          {/* End of Service section */}
+         <ServicesSection/>
 
           {/* Footer */}
 
